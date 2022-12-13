6829ce78ec1eae9132f32be79a9b14cd44661a5f
dragonnomada <dragonnomada123@gmail.com>
2022-12-13 16:42:40 -0600

    Sesión 23*

--- codigos/2203_Ejemplo_Protocolos.playground/Contents.swift
+++ codigos/2203_Ejemplo_Protocolos.playground/Contents.swift
@@ -1,3 +1,82 @@
 import UIKit
 
-var greeting = "Hello, playground"
+protocol LadradorProtocol {
+    
+    var ladridos: [String] { get }
+    
+    func ladrar()
+    
+}
+
+protocol CaminadorProtocol {
+    
+    var pasos: Int { get }
+    
+    func caminar()
+    
+}
+
+protocol ReportadorLadridosProtocol {
+    
+    func reportarLadridos(ladrador: LadradorProtocol)
+    
+}
+
+class Perro: LadradorProtocol, CaminadorProtocol {
+    
+    let nombre: String
+    let raza: String
+    
+    init(nombre: String, raza: String) {
+        self.nombre = nombre
+        self.raza = raza
+    }
+    
+    var ladridos: [String] = []
+    
+    func ladrar() {
+        ladridos.append("[Raza \(self.raza)] (\(self.nombre)) woof woof \(ladridos.count)")
+    }
+    
+    var pasos: Int = 0
+    
+    func caminar() {
+        pasos += 10
+    }
+    
+}
+
+class PerroReportador: ReportadorLadridosProtocol {
+    
+    func reportarLadridos(ladrador: LadradorProtocol) {
+        
+        for ladrido in ladrador.ladridos {
+            print(ladrido)
+        }
+        
+    }
+    
+}
+
+var perro1 = Perro(nombre: "Bombón", raza: "Frech Pudle")
+
+perro1.ladrar()
+perro1.ladrar()
+perro1.ladrar()
+
+perro1.caminar()
+perro1.caminar()
+
+var perro2 = Perro(nombre: "Choclo", raza: "Callejero")
+
+perro2.ladrar()
+perro2.ladrar()
+
+perro2.caminar()
+perro2.caminar()
+perro2.caminar()
+
+var reportadorPerros1 = PerroReportador()
+
+reportadorPerros1.reportarLadridos(ladrador: perro1)
+reportadorPerros1.reportarLadridos(ladrador: perro2)
