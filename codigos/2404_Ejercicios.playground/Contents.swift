5dc6bfd097a5f17dd0343ad3867857e1a78f2f86
dragonnomada <dragonnomada123@gmail.com>
2022-12-15 12:49:26 -0600

    Sesión 24 - Ejercicios

--- codigos/2404_Ejercicios.playground/Contents.swift
+++ codigos/2404_Ejercicios.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Manejo de Errores
+
+// - Diseña una función capaz de arrojar errores
+//   que reciba una lista de productos de la estructura Producto
+//   y devuelva el precio total de todos los productos.
+//
+//   Ocurrirá un error sí:
+//   > Existe un producto sin existencias
+//   > Un producto tiene un id mayor a 100
+//   > El precio es mayor a 1_000
+//
+//   Nota: Deberás crear una enumeración para cada posible error
+//
+//   Crea diversas pruebas unitarias dónde:
+//   * No falle
+//   * Exista un producto sin existencias
+//   * Exista un producto con id mayor a 100
+//   * Exista un producto con precio mayor a 1_000
+//
+//   Utiliza do { try } catch {  } para validar cada error
+
+// Subscripts
+
+// -
+
+// Concurrencia
+
+// -
