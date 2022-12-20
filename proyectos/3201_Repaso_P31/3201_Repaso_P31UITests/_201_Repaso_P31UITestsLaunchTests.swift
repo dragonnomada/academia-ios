38d3f68336bfc1a0c26333593b5599a547c8eee3
dragonnomada <dragonnomada123@gmail.com>
2022-12-20 10:28:13 -0600

    Sesión 32 - Update P31

---
+++ proyectos/3201_Repaso_P31/3201_Repaso_P31UITests/_201_Repaso_P31UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _201_Repaso_P31UITestsLaunchTests.swift
+//  3201_Repaso_P31UITests
+//
+//  Created by User on 20/12/22.
+//
+
+import XCTest
+
+final class _201_Repaso_P31UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
