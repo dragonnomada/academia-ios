b669b25bd258fe8c36eb4d3aa6df9a59d1d573cf
dragonnomada <dragonnomada123@gmail.com>
2022-12-21 17:28:48 -0600

    Sesión 33

---
+++ proyectos/3303_Apoyo_P33/3303_Apoyo_P33/RegistroEmpleadoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistroEmpleadoViewController.swift
+//  3303_Apoyo_P33
+//
+//  Created by User on 21/12/22.
+//
+
+import UIKit
+
+class RegistroEmpleadoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
