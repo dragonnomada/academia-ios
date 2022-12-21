c0a78b3840a000f7cc72dd55ffc8ae2f3e652cb1
dragonnomada <dragonnomada123@gmail.com>
2022-12-21 15:02:38 -0600

    Sesión 33 - P33

---
+++ proyectos/3302_Core_Data/3302_Core_Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3302_Core_Data
+//
+//  Created by User on 21/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
