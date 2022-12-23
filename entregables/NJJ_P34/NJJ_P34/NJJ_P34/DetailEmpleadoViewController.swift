fea862628d1129d817579464e1076ba84f688d0a
joelbrayan22 <joelnavorjimenez@gmail.com>
2022-12-23 10:31:51 -0600

    NJJ_P34

--- entregables/NJJ_P34/NJJ_P34/NJJ_P34/DetailEmpleadoViewController.swift
+++ entregables/NJJ_P34/NJJ_P34/NJJ_P34/DetailEmpleadoViewController.swift
@@ -1,7 +1,8 @@
 //
 //  DetailEmpleadoViewController.swift
 //  NJJ_P34
-//
+// Joel Brayan Navor Jimenez (joelnavorjimenez@gmail.com)
+// Combine segue manual
 //  Created by MacBook on 22/12/22.
 //
 
@@ -9,12 +10,17 @@ import UIKit
 import Combine
 
 class DetailEmpleadoViewController: UIViewController {
-    
-    
-    
-    var empleado: Empleado?
+    //Creo a mi empleado Subject
+    var empleadoSubject:  PassthroughSubject<Empleado, Never>?
+    //creo a mi tipo empleado
+    var empleadoSeleccionado: Empleado?
 
     
+    @IBAction func cancelar(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    //Conexiones de los textfield al storyboard
     @IBOutlet weak var nombreTextField: UITextField!
     
     
@@ -22,20 +28,33 @@ class DetailEmpleadoViewController: UIViewController {
     
     
     @IBOutlet weak var sueldoTextField: UITextField!
-    
-    
-    @IBAction func botonEnviar(_ sender: Any) {
-    }
-    
+    //Funciòn que se ejecuta al cargarse la primer pantalla
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let empleado = empleado {
-            nombreTextField.text = empleado.nombre
-            puestoTextField.text = empleado.puesto
-            sueldoTextField.text? = "\(empleado.sueldo)"
+        //Le digo a mi pantalla que actualize los valores de mis textInput con los datos enviados desde mi tabla en la pantalla principal
+        nombreTextField.text = "\(empleadoSeleccionado!.nombre)"
+        puestoTextField.text = "\(empleadoSeleccionado!.puesto)"
+        sueldoTextField.text = "\(empleadoSeleccionado!.sueldo)"
         }
-    }
+    //Botòn enviar que actualizara y guardara los datos de mi arreglo para mostrarlos de forma actualizada en mi tabla al inicio de la aplicaciòn
+    @IBAction func botonEnviar(_ sender: Any) {
+        //indicamos que datos son los que pueden modificarse con los datos agregados en el textField
+        let id: Int = empleadoSeleccionado!.id
+        let nombre: String = nombreTextField.text!
+        let puesto: String = puestoTextField.text!
+        let sueldo: Double = Double(sueldoTextField.text!)!
+        let antiguedad: Int = empleadoSeleccionado!.antiguedad
+        //Creaciòn del empleado acutalizado con los datos nuevos
+        let empleadoActualizado = Empleado(id: id, nombre: nombre, puesto: puesto, sueldo: sueldo, antiguedad: antiguedad)
+              // empleadoSubject que enviara los datos de de mi empleado actualizado
+              if let empleadoSubject = empleadoSubject {
+                  empleadoSubject.send(empleadoActualizado)
+              }
+                //al presionar mi botòn regresa a mi pantalla anterior .pop
+              dismiss(animated: true)
+          }
     
+    }
 
-}
+
+
--- entregables/NJJ_P34/NJJ_P34/NJJ_P34/ViewController.swift
+++ entregables/NJJ_P34/NJJ_P34/NJJ_P34/ViewController.swift
@@ -7,28 +7,50 @@
 //
 
 import UIKit
-import Combine
+import Combine //Importaciòn combine
 
 class ViewController: UIViewController {
-    
-    var empleadoSubject = PassthroughSubject<String, Never>()
-    
-    var empleadoSeleccionado: Empleado? = nil
-    
+    //Intancia de mi empleadoSubject
+    var empleadoSubject = PassthroughSubject<Empleado, Never>()
+    //Instancia de mi empleadoSubscriber
+    var empleadoSubscriber: AnyCancellable?
+    //Instancia del empleado seleccionado del tipo empleado
+    var empleadoSeleccionado: Empleado?
+    //Creaciòn de arreglo de empleados
+    var empleados : [Empleado] = [
+        Empleado(id: 1, nombre: "Joel", puesto: "Desarrollador", sueldo: 33.20, antiguedad: 1),
+        Empleado(id: 2, nombre: "Hernesto", puesto: "Recursos Humanos", sueldo: 330.20, antiguedad: 3)
+    ]
+    //Conexion a mi tabla
     @IBOutlet weak var myTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //Le indicamos que nuestro data source y nuestro delegate forman parte de nuuestra misma clase
         myTableView.delegate = self
         myTableView.dataSource = self
+        //empleadoSubscriber va a recibir los datos de empleado subject y guardara en una variable (empleadoRecibido) para despues asignarle los valores a empleadoSubscriber
+        empleadoSubscriber = empleadoSubject.sink(receiveValue: { empleadoRecibido in
+            //vemos que datos recibimos
+            print(empleadoRecibido)
+            //Recorremos a empleados para emparejar los datos recibidos con el indice de mi arreglo de empleados y asignamos los nuevos valores
+            for i in 0..<self.empleados.count {
+                if empleadoRecibido.id == self.empleados[i].id {
+                    self.empleados[i] = empleadoRecibido
+                }
+            }
+            //Recargamos la tabla con los datos actualizados
+            self.myTableView.reloadData()
+                    
+                })
         
     }
+    //Preparaciòn del segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "mySegue" {
             if let detailEmpleado = segue.destination as? DetailEmpleadoViewController {
-                if let empleadoSeleccionado = sender as? Empleado {
-                    detailEmpleado.empleado = empleadoSeleccionado
+                detailEmpleado.empleadoSubject = self.empleadoSubject
+                detailEmpleado.empleadoSeleccionado = self.empleadoSeleccionado
                 }
             }
             
@@ -36,9 +58,7 @@ class ViewController: UIViewController {
         
     }
     
-    
-}
-
+//Estructura empleado con los datos del empleado
 struct Empleado {
     let id: Int
     let nombre: String
@@ -46,40 +66,45 @@ struct Empleado {
     let sueldo: Double
     let antiguedad: Int
 }
-
+//Repositorio empleado para la creaciòn del singletron main
 class EmpleadosRepository {
     static let main = EmpleadosRepository()
     
-    var empleados : [Empleado] = [Empleado(id: 1, nombre: "Joel", puesto: "Desarrollador", sueldo: 33.20, antiguedad: 1)]
+   
 }
-
+//DataSource y Delegate de mi tabla
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
+    //Configuracion del numero de filas por secciòn
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return EmpleadosRepository.main.empleados.count
+        return empleados.count
     }
-    
+    //Configuracion de los datos en celda
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        let empleado = EmpleadosRepository.main.empleados[indexPath.row]
-        cell.textLabel?.text = empleado.nombre
-        cell.detailTextLabel?.text = empleado.puesto
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Empleados"
-    }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let empleado = EmpleadosRepository.main.empleados[indexPath.row]
-        empleadoSeleccionado = empleado
-        performSegue(withIdentifier: "mySegue", sender: nil)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellD")!
+            
+        cell.textLabel?.text = "\(empleados[indexPath.row].nombre)"
+        cell.detailTextLabel?.text = "\(empleados[indexPath.row].puesto)"
+            return cell
+        }
+        //Configuraciòn del nombre de mi secciòn
+        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            return "Empleados"
+        }
+        //Configuraciòn del numero de seccionnes
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        //Por medio de didSelectRow le indicamos que a que empleado le queremos modificar los datos
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            
+            //indicamos el indice del empleado en la celda seleccionada
+            empleadoSeleccionado = empleados[indexPath.row]
+            //Ejecutamos el segue para pasar a la pantalla de detalles del empleado
+            performSegue(withIdentifier: "mySegue", sender: nil)
         }
         
     }
     
-
+    
+    
 
