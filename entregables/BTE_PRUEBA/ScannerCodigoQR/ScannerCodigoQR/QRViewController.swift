816b1e46c5c3c1599a1af13a5d4ab3d275e7dd83
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2022-12-27 13:45:10 -0600

    Eduardo Batista Prueba

---
+++ entregables/BTE_PRUEBA/ScannerCodigoQR/ScannerCodigoQR/QRViewController.swift
@@ -0,0 +1,86 @@
+import UIKit
+import AVFoundation
+
+class QRScannerController: UIViewController {
+    
+    @IBOutlet weak var cobrAppTextField: UILabel!
+    @IBOutlet weak var escaneaQRTextField: UILabel!
+    
+   
+    var captureSession = AVCaptureSession()
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+    
+    private let supportedCodeTypes = [AVMetadataObject.ObjectType.qr]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+            print("Error al abrir la camara del dispositivo")
+            return
+        }
+        
+        do {
+            
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            captureSession.addInput(input)
+            
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession.addOutput(captureMetadataOutput)
+            
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            
+            captureSession.startRunning()
+            
+            view.bringSubviewToFront(escaneaQRTextField)
+            view.bringSubviewToFront(cobrAppTextField)
+            
+            qrCodeFrameView = UIView()
+            
+            if let qrcodeFrameView = qrCodeFrameView {
+                qrcodeFrameView.layer.borderColor = UIColor.yellow.cgColor
+                qrcodeFrameView.layer.borderWidth = 2
+                view.addSubview(qrcodeFrameView)
+                view.bringSubviewToFront(qrcodeFrameView)
+            }
+            
+        } catch {
+            print(error)
+            return
+        }
+    }
+}
+
+extension QRScannerController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            escaneaQRTextField.text = "El codigo QR no es detectado"
+            return
+        }
+        
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if supportedCodeTypes.contains(metadataObj.type) {
+
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                escaneaQRTextField.text = metadataObj.stringValue
+            }
+        }
+    }
+    
+}
+
---
+++ entregables/BTE_PRUEBA/ScannerCodigoQR/ScannerCodigoQR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScannerCodigoQR
+//
+//  Created by MacBook Pro on 27/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
