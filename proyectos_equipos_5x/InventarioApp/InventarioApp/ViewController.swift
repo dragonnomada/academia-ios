ce53a1b1ccbef09f6a41a8db03997e05f083d2ab
JonathanAmador12 <jamadorh91@gmail.com>
2023-01-03 15:57:46 -0600

    inventarioApp

---
+++ proyectos_equipos_5x/InventarioApp/InventarioApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InventarioApp
+//
+//  Created by MacBook  on 03/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
