f8896f3bac7de2a173321c9a5bf5309a4d2bdcff
JonathanAmador12 <jamadorh91@gmail.com>
2023-01-05 13:38:58 -0600

    Actualizando las vistas

---
+++ proyectos_equipos_5x/InventarioApp/InventarioApp/Views/AddProductViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddViewController.swift
+//  InventarioApp
+//
+//  Created by MacBook  on 05/01/23.
+//
+
+import UIKit
+
+class AddProductViewController: UIViewController {
+
+    @IBOutlet weak var addProductImage: UIImage!
+    @IBOutlet weak var addProductName: UITextField!
+    @IBOutlet weak var addProductDescripcion: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func cancelar(_ sender: Any) {
+        performSegue(withIdentifier: "", sender: self)
+    }
+    
+    @IBAction func agregar(_ sender: Any) {
+        //
+    }
+
+}
