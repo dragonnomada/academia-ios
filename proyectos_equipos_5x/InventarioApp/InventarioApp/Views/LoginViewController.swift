251564eee4bb4c134d57a2466e5b16fceae8046f
dragonnomada <dragonnomada123@gmail.com>
2023-01-06 09:08:00 -0600

    Corrección del controlador y funcionalidad del login

--- proyectos_equipos_5x/InventarioApp/InventarioApp/Views/LoginViewController.swift
+++ proyectos_equipos_5x/InventarioApp/InventarioApp/Views/LoginViewController.swift
@@ -18,10 +18,31 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        InventarioController.shared.inventarioLoginDelegate = self
     }
     
     @IBAction func submitClicked(_ sender: Any){
-        performSegue(withIdentifier: "Login-Home-Segue", sender: self)
+        guard let usuario = usuarioTextField.text else { return }
+        
+        guard let password = passwordTextField.text else { return }
+        
+        InventarioController.shared.getUsuario(nombre: usuario, password: password)
     }
    
+}
+
+extension LoginViewController: InventarioLoginDelegate {
+    func login(usuarioIniciado: UsuarioEntity) {
+        performSegue(withIdentifier: "Login-Home-Segue", sender: self)
+    }
+    
+    func loginError(loginError error: String) {
+        let alert = UIAlertController(title: "Error", message: error, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "ok", style: .default))
+        
+        self.present(alert, animated: true)
+    }
+    
+    
 }
