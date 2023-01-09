0003b16e8b883b658e66e19994fb214db90d40b5
dragonnomada <dragonnomada123@gmail.com>
2023-01-09 16:00:12 -0600

    Actualizando Proyecto NominApp

---
+++ codigos_6x/6104_Todo_MVVM_Nib/6104_Todo_MVVM_Nib/Views/TodoHomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodoHomeViewController.swift
+//  6104_Todo_MVVM_Nib
+//
+//  Created by Dragon on 09/01/23.
+//
+
+import UIKit
+
+class TodoHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
