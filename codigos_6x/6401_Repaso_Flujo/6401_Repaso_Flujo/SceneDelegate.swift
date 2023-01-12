6cee3d2362e922633b70182bf694a8c050494a02
dragonnomada <dragonnomada123@gmail.com>
2023-01-12 13:39:52 -0600

    Semana 6 Día 4 - Flujo MVVM

---
+++ codigos_6x/6401_Repaso_Flujo/6401_Repaso_Flujo/SceneDelegate.swift
@@ -0,0 +1,93 @@
+//
+//  SceneDelegate.swift
+//  6401_Repaso_Flujo
+//
+//  Created by Dragon on 12/01/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate, UINavigationControllerDelegate {
+
+    var window: UIWindow?
+
+    private lazy var model: MyModel = {
+        let model = MyModel()
+        
+        // TODO: Cargamos datos al modelo
+        
+        return model
+    }()
+    
+    private lazy var viewModel: MyViewModel = {
+        
+        let viewModel = MyViewModel(model: self.model)
+        
+        // TODO: Configurar el View-Model
+        
+        return viewModel
+        
+    }()
+    
+    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        
+        if let viewController = viewController as? ViewController {
+            viewController.viewModel = self.viewModel
+        }
+        
+        /*if let frutasHomeViewController = viewController as? FrutasHomeViewController {
+            frutasHomeViewController.viewModel = self.homeViewModel
+        }*/
+        
+    }
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if let navigationController = self.window?.rootViewController as? UINavigationController {
+            
+            navigationController.delegate = self
+            
+        }
+        
+        /*if let viewController = self.window?.rootViewController as? ViewController {
+            
+            viewController.viewModel = self.viewModel
+            
+        }*/
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+
+
+}
+
